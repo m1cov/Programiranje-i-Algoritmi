ba8499e6c04b163b601cbedd04976f966fdbdf55
m1cov <rade.micov@gmail.com>
2022-12-10 14:24:20 +0100

    Lab 7

---
+++ Laboratoriski 7/Lab7_1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define max 100
+
+int main()
+{
+
+    int n, m, br;
+    int mat[max][max];
+    float sr = 0;
+
+    printf("Vnesi golemina na matricata\n");
+    scanf("%d %d", &n, &m);
+
+    if (n > max || m > max)
+        return -1;
+
+    br = m * n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+
+            scanf("%d", &mat[i][j]);
+            sr += mat[i][j];
+        }
+    }
+
+    sr /= br;
+    br = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (mat[i][j] > sr)
+            {
+                br++;
+                printf("%d ", mat[i][j]);
+            }
+        }
+    }
+
+    printf("\n%d", br);
+
+    return 0;
+}
---
+++ Laboratoriski 7/Lab7_2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define max 100
+
+int main()
+{
+
+    int n, m;
+    int a[max][max], b[max][max];
+
+    printf("Vnesi ja goleminata na matricite\n");
+    scanf("%d %d", &n, &m);
+
+    if (n > max || m > max)
+        return -1;
+
+    printf("Vnesi ja prvata matrica\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Vnesi ja vtorata matrica\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &b[i][j]);
+
+    int br = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == b[i][j])
+            {
+                br++;
+                printf("Imaat isti elementi na pozicijata %d %d i elementot e %d \n", i, j, a[i][j]);
+            }
+        }
+    }
+    printf("Imaat %d isti elementi", br);
+
+    return 0;
+}
