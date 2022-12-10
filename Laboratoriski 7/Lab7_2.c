dda3b02fbae1ed2486a04f9ccfa3e7d2e0a658f9
m1cov <rade.micov@gmail.com>
2022-12-10 14:25:12 +0100

    Lab 7

--- Laboratoriski 7/Lab7_2.c
+++ Laboratoriski 7/Lab7_2.c
@@ -15,13 +15,21 @@ int main()
 
     printf("Vnesi ja prvata matrica\n");
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < m; j++)
+        {
             scanf("%d", &a[i][j]);
+        }
+    }
 
     printf("Vnesi ja vtorata matrica\n");
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < m; j++)
+        {
             scanf("%d", &b[i][j]);
+        }
+    }
 
     int br = 0;
 
