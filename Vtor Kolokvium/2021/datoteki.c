c864964c386fcb7ee93390ef8e6a821769ff6096
m1cov <rade.micov@gmail.com>
2022-12-26 22:45:00 +0100

    da

--- Vtor Kolokvium/2021/datoteki.c
+++ Vtor Kolokvium/2021/datoteki.c
@@ -7,6 +7,7 @@
 се даде упатство за користење. Линиите во датотеките имаат најмногу 250 знаци.*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -14,7 +15,66 @@ int main(int argc, char *argv[])
 {
 
     FILE *fin, *fout;
-    int niza[251];
+    char niza[251];
+    int golemi = 0, samoglaski = 0;
+    int rgolemi = 0, rsamoglaski = 0;
+    int i = 0;
+    char c;
+
+    if (argc != 3)
+    {
+        printf("Povikot treba da ima forma %s <in_dat> <out_dat\n", argv[0]);
+        return 0;
+    }
+    else if ((fin = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Ne moze da se otvori vleznata datoteka\n");
+        return 0;
+    }
+    else if ((fout = fopen(argv[2], "w")) == NULL)
+    {
+        printf("Ne moze da se otvori izleznata datoteka\n");
+        return 0;
+    }
+
+    while ((c = fgetc(fin)) != EOF)
+    {
+        if (c != '\n')
+        {
+            niza[i] = c;
+            i++;
+            if (isupper(c))
+            {
+                golemi++;
+                rgolemi++;
+            }
+            c = toupper(c);
+
+            if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            {
+
+                samoglaski++;
+                rsamoglaski++;
+            }
+        }
+        else
+        {
+
+            if (rgolemi > rsamoglaski)
+            {
+                niza[i] = 0;
+                fprintf(fout, "%s\n", niza);
+            }
+
+            rsamoglaski = 0;
+            rgolemi = 0;
+            i = 0;
+        }
+    }
+
+    fprintf(fout, "%d %d\n", golemi, samoglaski);
+    fclose(fin);
+    fclose(fout);
 
     return 0;
 }
