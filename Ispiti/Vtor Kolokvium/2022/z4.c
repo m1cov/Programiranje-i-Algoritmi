8feceea84e0ccb65aa54a652704d25b1c4bcebff
m1cov <rade.micov@gmail.com>
2023-01-22 20:06:14 +0100

    2022

--- Ispiti/Vtor Kolokvium/2022/z4.c
+++ Ispiti/Vtor Kolokvium/2022/z4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +17,30 @@ int main(int argc, char *argv[])
     if ((outdat = fopen(argv[2], "w")) == NULL)
         return -1;
 
-    
+    while ((c = fgetc(indat)) != EOF)
+    {
+
+        if (isalnum(c))
+        {
+
+            if (!isalpha(c))
+                fprintf(outdat, "\n%c\n", c);
+            else
+                fprintf(outdat, "%c", c);
+        }
+
+        if (!isalnum(c))
+        {
+
+            if (c == '\n')
+                continue;
+            else
+                fprintf(outdat, "%c", c);
+        }
+    }
+
+    fclose(indat);
+    fclose(outdat);
 
     return 0;
 }
